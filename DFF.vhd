b4e091a7ccdcc87dd521d5249c4c1b0847ec66b8
abhishek konale <abhishek konale>
2016-10-13 16:02:46 +0530

    templates

--- DFF.vhd
+++ DFF.vhd
@@ -7,7 +7,7 @@ begin
 	FF :process(clk) is
 	begin
 		if clk='0' then
-			Q<=D after 5 ns;
+			Q<=D ;
 		end if;
 	end process FF;
 	QN<=not Q;
@@ -17,38 +17,4 @@ end architecture behave;
 
 
 
-entity testbench_DFF is
 
-end entity testbench_DFF;
-
-architecture test of testbench_DFF is
-
-component DFF is
-	port (D,clk:in bit; Q:inout bit;QN:out bit:='1');
-end component;
-signal D,clk,Q,QN:bit;
-
-begin 
-
-	DFF_1: DFF port map(D,clk,Q,QN);
-	clk<= not clk after 10 ns;
-	
-	testcase :process is
-	begin
-		D<='1';
-		wait for 10 ns;
-
-		D<='0';
-		wait for 30 ns;
-
-		D<='1';
-		wait for 70 ns;
-
-		D<='0';
-		wait for 95 ns;
-
-		wait;
-		
-	end process testcase;
-
-end architecture test;
