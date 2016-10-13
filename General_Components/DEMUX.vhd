397b499f54635da399e0a3bb12d6298671bf0740
abhishek konale <abhishek konale>
2016-10-14 00:38:15 +0530

    MUX-DEMUX complete

---
+++ General_Components/DEMUX.vhd
@@ -0,0 +1,22 @@
+entity DEMUX is
+ 	port(Sel: in std_logic_vector(2 downto 0);  Y: out std_logic_vector(15 downto 0));
+end entity DEMUX;
+
+architecture behave of DEMUX is
+
+Y  <=	  "00000001" when Sel = "000" else  
+          "00000010" when Sel = "001" else  
+          "00000100" when Sel = "010" else  
+          "00001000" when Sel = "011" else  
+          "00010000" when Sel = "100" else  
+          "00100000" when Sel = "101" else  
+          "01000000" when Sel = "110" else  
+          "10000000" when Sel = "111" else  
+          "----------------";  
+
+end architecture behave;
+
+
+
+
+
--- General_Components/General_Components.vhd
+++ General_Components/General_Components.vhd
@@ -11,7 +11,13 @@ component SET_POS_ZERO is
 	port ( X : in std_logic_vector (7 downto 0); POS:in std_logic_vector (2 downto 0); Y :out std_logic_vector (2 downto 0));  
 end component;  
 
+component MUX_16_8 is
+ 	port(X0,X1,X2,X3,X4,X5,X6,X7: in std_logic_vector(15 downto 0); Sel: in std_logic_vector(2 downto 0);  Y: out std_logic_vector(15 downto 0));
+end component MUX_16_8;
 
+component DEMUX is
+ 	port(Sel: in std_logic_vector(2 downto 0);  Y: out std_logic_vector(15 downto 0));
+end component;
 	
 end General_Components;
 
