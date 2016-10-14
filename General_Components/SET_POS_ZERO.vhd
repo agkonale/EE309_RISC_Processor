64e7bf6acc67b845083a61c5e4719d4122fdf71e
abhishek konale <abhishek konale>
2016-10-14 19:53:04 +0530

    Memory complete

--- General_Components/SET_POS_ZERO.vhd
+++ General_Components/SET_POS_ZERO.vhd
@@ -2,20 +2,20 @@ library ieee;
 use ieee.std_logic_1164.all;  
  
 entity SET_POS_ZERO is  
-	port ( X : in std_logic_vector (7 downto 0); POS:in std_logic_vector (2 downto 0); Y :out std_logic_vector (2 downto 0));  
+	port (X : in std_logic_vector (7 downto 0); POS:in std_logic_vector (2 downto 0); init :in std_logic; Y :out std_logic_vector (7 downto 0));  
 end SET_POS_ZERO;  
 
 architecture Struct of SET_POS_ZERO is  
 begin  
 
-begin  
-     Y  <="11111110" when POS = "000" else  
-          "11111100" when POS = "001" else  
-          "11111000" when POS = "010" else  
-          "11110000" when POS = "011" else  
-          "11100000" when POS = "100" else  
-          "11000000" when POS = "101" else  
-          "10000000" when POS = "110" else  
-          "00000000" when POS = "111" else  
+     Y  <=X when init = '1' else
+     	  X and "11111110" when POS = "000" else  
+          X and "11111100" when POS = "001" else  
+          X and "11111000" when POS = "010" else  
+          X and "11110000" when POS = "011" else  
+          X and "11100000" when POS = "100" else  
+          X and "11000000" when POS = "101" else  
+          X and "10000000" when POS = "110" else  
+          X and "00000000" when POS = "111" else  
           "--------";  
 end Struct; 
